166810b6110dbf0964076082f2f5ffffc46d20bb
Reinaldo <reinaldo@coderscolony.com>
2019-08-14 10:59:31 +0700

    Update CSS & JS

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,38 +1,6 @@
 <!DOCTYPE html>
 <html>
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>MyRestaurantReservation</title>
-  <!-- Tell the browser to be responsive to screen width -->
-  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-  <link rel="stylesheet" href="../node_modules/admin-lte/bower_components/bootstrap/dist/css/bootstrap.min.css">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="../node_modules/admin-lte/bower_components/font-awesome/css/font-awesome.min.css">
-  <!-- Ionicons -->
-  <link rel="stylesheet" href="../node_modules/admin-lte/bower_components/Ionicons/css/ionicons.min.css">
-  <!-- DataTables -->
-  <link rel="stylesheet" href="../node_modules/admin-lte/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="../node_modules/admin-lte/dist/css/AdminLTE.min.css">
-  <!-- AdminLTE Skins. Choose a skin from the css/skins folder instead of downloading all of them to reduce the load. -->
-  <link rel="stylesheet" href="../node_modules/admin-lte/dist/css/skins/_all-skins.min.css">
-  <!-- jQuery 3 -->
-  <script src="../node_modules/admin-lte/bower_components/jquery/dist/jquery.min.js"></script>
-  <!-- Bootstrap 3.3.7 -->
-  <script src="../node_modules/admin-lte/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-  <!-- Slimscroll -->
-  <script src="../node_modules/admin-lte/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
-  <!-- FastClick -->
-  <script src="../node_modules/admin-lte/bower_components/fastclick/lib/fastclick.js"></script>
-  <!-- AdminLTE App -->
-  <script src="../node_modules/admin-lte/dist/js/adminlte.min.js"></script>
-  <!-- iCheck -->
-  <script src="../node_modules/admin-lte/plugins/iCheck/icheck.min.js"></script>
-  <!-- AdminLTE for demo purposes -->
-  <script src="../node_modules/admin-lte/dist/js/demo.js"></script>
-  
-</head>
+  @extends('script')
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
